dd04759f3bf7eb1165d63628c34b180a52731476
Luiz Henrique Marques Gonçalves <65735970+luizmgon@users.noreply.github.com>
2023-04-13 20:33:09 -0300

    Update test_unit.py

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -68,13 +68,14 @@ def test_8_chars_with_no_upper_case_password_should_raise_exception():
         # when
         validator.password_validate()
 
-        
+
 def test_8_chars_with_wrong_especial_character_password_should_raise_exception():
     # given
     validator = PasswordValidator(content="1abAb1^/")
     with pytest.raises(Exception):  # then
         # when
         validator.password_validate()
+
 
 def test_8_chars_with_prohibited_especial_character_password_should_raise_exception():
     # given
