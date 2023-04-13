2d6058a135bf94c8c7a65d492c7838ad6765a410
Luiz Henrique Marques Gonçalves <65735970+luizmgon@users.noreply.github.com>
2023-04-13 20:18:56 -0300

    Update string_validators.py

--- simple_web_app/string_validators.py
+++ simple_web_app/string_validators.py
@@ -57,6 +57,7 @@ class EspecialCharacterValidator(Validator):
         self.especial_set = set(punctuation)
         self.especial_set.remove("^")
         self.especial_set.remove("/")
+        self.especial_set.remove("~")
 
     def validate(self, content):
         content_set = set(content)
